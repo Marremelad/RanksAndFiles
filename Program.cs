f6406afc4831ec05ba2dbc732f655bd524491324
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-09-12 15:19:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace RanksAndFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isNumber;
+
+            do {
+                Console.WriteLine("Please enter size of board");
+                isNumber = int.TryParse(Console.ReadLine(), out int boardSize);
+
+                var myArray = new string[boardSize, boardSize];
+
+                for (int i = boardSize; i > 0; i--) {
+                    for (int j = 0; j < boardSize; j++) {
+                        string boardPosition = $"{(char)(j + 97)}{i}";
+                        myArray[i - 1, j] = boardPosition;
+                    }
+                }
+
+                for (int i = boardSize; i > 0; i--) {
+                    for (int j = 0; j < boardSize; j++) Console.Write(myArray[i - 1, j]);
+                    Console.WriteLine();
+                }
+            } while (!isNumber);
+        }
+    }
+}
